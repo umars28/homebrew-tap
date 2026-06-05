0dac4b603bc2d0daf520c00c3077d549a4784d21
Umar Sabirin <usabirin26@dev1.idserve.net>
2026-06-06 01:46:49 +0700

    Use git checkout for sshm formula

--- Formula/sshm.rb
+++ Formula/sshm.rb
@@ -1,8 +1,9 @@
 class Sshm < Formula
   desc "SSHM TUI"
   homepage "https://github.com/umars28/sshm"
-  url "https://github.com/umars28/sshm/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "33c8f92036be1007a754ae120499a6dae37b147a7b055fcd7cef9ba62926902a"
+  url "https://github.com/umars28/sshm.git",
+      tag: "v0.1.1"
+  head "https://github.com/umars28/sshm.git", branch: "main"
   license "Apache-2.0"
 
   depends_on "go" => :build
